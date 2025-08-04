b7d60a9177145212d73113ee026f554cf79eb6d0
walfgenxx <leanwalf10@gmail.com>
2025-08-04 11:08:36 +0100

    Update util.go

--- util/util.go
+++ util/util.go
@@ -3,7 +3,6 @@ package util
 import (
 	"encoding/json"
 	"fmt"
-	"math/rand"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -100,23 +99,4 @@ func ExtractClaimableTime(p xdr.ClaimPredicate) (time.Time, bool) {
 	default:
 		return time.Time{}, false
 	}
-}
-
-// GetCompetitiveFee returns a competitive fee to outperform other bots
-func GetCompetitiveFee(baseAmount int64, isUrgent bool) int64 {
-	rand.Seed(time.Now().UnixNano())
-	
-	if isUrgent {
-		// For critical claiming operations, use maximum competitive fees
-		return baseAmount + int64(rand.Intn(5000000)) // Add 0.5 PI randomness
-	}
-	
-	// For regular operations
-	return baseAmount + int64(rand.Intn(1000000)) // Add 0.1 PI randomness
-}
-
-// CalculateOptimalTiming returns the optimal time to start operations
-func CalculateOptimalTiming(unlockTime time.Time) time.Time {
-	// Start 100ms before unlock to beat competitors
-	return unlockTime.Add(-100 * time.Millisecond)
 }
